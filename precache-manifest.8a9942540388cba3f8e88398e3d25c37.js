544fcc22513ce9bcf2de458f5f37008125a58bd7
Tan Jiaqing <jiaqing.tan4@gmail.com>
2020-08-13 21:59:21 +0800

    Updates

---
+++ precache-manifest.8a9942540388cba3f8e88398e3d25c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed8fd36baca314a1f60adeeb7a35930",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "46ac6c412a9acc4b0505",
+    "url": "/portfolio/static/css/2.dc38823f.chunk.css"
+  },
+  {
+    "revision": "732eb31c2175984bef5f",
+    "url": "/portfolio/static/css/main.0f59db8e.chunk.css"
+  },
+  {
+    "revision": "46ac6c412a9acc4b0505",
+    "url": "/portfolio/static/js/2.b0f7dd54.chunk.js"
+  },
+  {
+    "revision": "d1f552577fbd0859131edd51f65f75d8",
+    "url": "/portfolio/static/js/2.b0f7dd54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732eb31c2175984bef5f",
+    "url": "/portfolio/static/js/main.a3c6fd3b.chunk.js"
+  },
+  {
+    "revision": "e5275a332819a98eec24",
+    "url": "/portfolio/static/js/runtime-main.3eaf5bd3.js"
+  }
+]);
